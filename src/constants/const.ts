d2640a5e9c323dd608d21693ae22e88c1c281f11
Янбухтин Максим <myanbukhtin@gmail.com>
2023-10-19 19:57:40 +0300

    homework module3-task1

--- src/constants/const.ts
+++ src/constants/const.ts
@@ -2,3 +2,16 @@ export const Setting = {
   OffersCount: 312,
 } as const;
 
+export const enum AppRoute {
+  Main = '/',
+  Login = '/login',
+  Favorites = '/favorites',
+  Offer = '/offer/:id'
+}
+
+export const enum AuthorizationStatus {
+  Auth = 'AUTH',
+  NoAuth = 'NO_AUTH',
+  Unknown = 'UNKNOWN'
+}
+
